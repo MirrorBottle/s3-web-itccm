4d4739fa9a04db20b81b6706432c1ee40a94dd58
Alfinorihsan <=alfinorihsan@gmail.com>
2022-11-06 15:27:18 +0800

    add crud for examinition

--- company/examinations/index.php
+++ company/examinations/index.php
@@ -16,31 +16,42 @@
     <div class="card-body">
       <div class="table-wrapper">
 
-        <table class="datatable">
+      <table class="datatable">
           <thead>
             <tr>
               <th>Status</th>
+              <th>Company</th>
               <th>Examination Number</th>
+              <th>Registration Number</th>
               <th>Standar</th>
               <th>Auditor</th>
               <th>Tanggal</th>
+              <th></th>
             </tr>
           </thead>
           <tbody>
             <?php foreach($examinations as $examination): ?>
               <tr>
                 <td><?= $examination->status_label ?></td>
+                <td><?= $examination->company_name ?></td>
+                <td><?= $examination->registration_number ?></td>
                 <td><?= $examination->examination_number ?></td>
                 <td><?= $examination->standard_name ?></td>
-                <td>
+                <td> 
                   <?php foreach($examination->auditors as $auditor): ?>
                     <span><?= $auditor->name ?></span><br>
                   <?php endforeach ?>
                 </td>
                 <td><?= format_date($examination->examination_start_date, "d/m/Y") ?> ~ <?= format_date($examination->examination_end_date, "d/m/Y") ?></td>
+                <td>
+                  <div class="p-1 d-flex align-items-center">
+                    <a href="./show.php?id=<?= $examination->id ?>" class="btn btn-sm btn-secondary mr-1">
+                      <i class="fa-solid fa-circle-info"></i>
+                    </a>
+                  </div>
+                </td>
               </tr>
             <?php endforeach ?>
-            
           </tbody>
         </table>
       </div>
