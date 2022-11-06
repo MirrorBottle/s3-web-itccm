a0e6c36f25b34fc2f27adfe586c18e56fdcff7ab
awansetyawan <alifm6331@gmail.com>
2022-11-06 14:18:36 +0800

    Profile Auditor and Readme

--- README.md
+++ README.md
@@ -18,6 +18,6 @@
 ## Anggota
 | FOTO       | NIM                     | NAMA               | BAGIAN                              |
 |------------|-------------------------|--------------------|-------------------------------------|
-| <img src="./docs/assets/awan.png" height="100"> | 2109106002          | Alif Maulana Setyawan   | Auditor              |
+| <img src="./docs/assets/awan.jpeg" height="100"> | 2109106002          | Alif Maulana Setyawan   | Auditor              |
 | <img src="./docs/assets/alfi.png" height="100"> | 2109106018          | Alfi Nor Ihsan   | Company              |
 | <img src="./docs/assets/bayu.png" height="100"> | 2109106026          | Bayu Setiawan   | Admin              |
--- auditor/profile/index.php
+++ auditor/profile/index.php
@@ -2,16 +2,101 @@
 
 <?php
   $auditor_id = $_SESSION['user']->auditor_id;
-  $examinations = examination_list("", ['with_auditors' => true, 'with_schedules' => true], $auditor_id);
+  
+  $auditor = find("auditors", $_SESSION['user']->auditor_id);
+
+  $qualifications = query("
+  SELECT
+    auditor_qualifications.*,
+    standards.name as standard_name
+  FROM
+    auditor_qualifications
+    JOIN standards ON auditor_qualifications.standard_id = standards.id
+    WHERE auditor_qualifications.auditor_id = $auditor_id
+  ");
+
 ?>
 
 <section>
-  <div class="card">
+<div class="card mb-3">
     <div class="card-header">
-      <h3>Profile</h3>
+      <div class="d-flex align-items-center justify-content-between">
+        <h3>Detail Auditor</h3>
+      </div>
     </div>
     <div class="card-body">
-        
+      <div class="row descriptions">
+        <div class="col col-2">
+          <img src="../../storage/auditors/<?= $auditor->photo ?>" alt="">
+        </div>
+        <div class="col col-10">
+          <div class="row descriptions">
+            <div class="col col-6">
+              <div class="description-item">
+                <div class="description-label">Nama Depan</div>
+                <div class="description-value"><?= $auditor->first_name ?></div>
+              </div>
+            </div>
+            <div class="col col-6">
+              <div class="description-item">
+                <div class="description-label">Nama Belakang</div>
+                <div class="description-value"><?= $auditor->last_name ?></div>
+              </div>
+            </div>
+            <div class="col col-12">
+              <div class="description-item">
+                <div class="description-label">Email</div>
+                <div class="description-value"><?= $auditor->email ?></div>
+              </div>
+            </div>
+            <div class="col col-12">
+              <div class="description-item">
+                <div class="description-label">No. Kontak</div>
+                <div class="description-value"><?= $auditor->phone_number ?></div>
+              </div>
+            </div>
+            <div class="col col-12">
+              <div class="description-item">
+                <div class="description-label">Tanggal Lahir</div>
+                <div class="description-value"><?= format_date($auditor->birthday) ?></div>
+              </div>
+            </div>
+            <div class="col col-12">
+              <div class="description-item">
+                <div class="description-label">Alamat</div>
+                <div class="description-value"><?= $auditor->address ?></div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+
+  <div class="card mb-3">
+    <div class="card-header">
+      <div class="d-flex align-items-center justify-content-between">
+        <h3>Kualifikasi Auditor</h3>
+      </div>
+    </div>
+    <div class="card-body">
+      <table id="auditor-qualifications" class="datatable">
+        <thead>
+          <tr>
+            <th>Standard</th>
+            <th>Tanggal Kadaluarsa</th>
+          </tr>
+        </thead>
+        <tbody>
+          <?php foreach ($qualifications as $qualification) : ?>
+            <tr>
+              <td><?= $qualification->standard_name ?></td>
+              <td><?= format_date($qualification->expiration_date) ?></td>
+            </tr>
+          <?php endforeach ?>
+
+        </tbody>
+      </table>
     </div>
   </div>
 </section>
